fb1a9fa221b185fa9543e2f5bd88c02bd0e86324
Naveed Ali <naveedali@folio3.com>
2023-10-13 16:06:24 +0500

    First commit

---
+++ app/src/main/java/folio3/android/template/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package folio3.android.template.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import folio3.android.template.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
